4d7bcc4475824bdf30d40055707c298f615c65f0
hasin0 <57817426+hasin0@users.noreply.github.com>
2023-05-31 15:57:14 +0100

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
+# This script installs Jenkins on an Ubuntu server
 
-sudo apt update -y
+# Update package lists
+sudo apt-get update
 
-sudo apt install default-jre -y
+# Install Java 11
+sudo apt-get install -y openjdk-11-jdk
 
-java -version
+# Download Jenkins key and add it to system
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
-sudo apt update -y
+# Add Jenkins to system package source list
+sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null'
 
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+# Update package lists again to include Jenkins and install it
+sudo apt-get update
+sudo apt-get install -y jenkins
 
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-
-sudo apt update -y
-
-sudo add-apt-repository universe -y
-
-sudo apt-get install jenkins -y
-
-sudo service jenkins start
+# Verify Jenkins is installed and working
+sudo systemctl status jenkins
 
 cat /var/lib/jenkins/secrets/initialAdminPassword
 
